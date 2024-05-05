5050d7b3b809160fc1d272190d8166435ad22219
Abdelrhman Mostafa <abdelrhman.m.hessain@gmail.com>
2024-05-05 11:10:17 +0300

    added function to convert & assignment text

--- simulateRecursion/main.cpp
+++ simulateRecursion/main.cpp
@@ -4,6 +4,18 @@
 #include <iostream>
 #include <stack>
 
+using namespace std;
+
+/*
+int F(int n)
+{
+	if (n <= 1) return 1;
+	int a = n * F(n - 1) + 7 * F(n / 2);
+	int b = F(n - 3 - a % 2);
+	return a + b;
+}
+*/
+
 
 int main(){
 
